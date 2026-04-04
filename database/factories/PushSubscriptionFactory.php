674163cb4b72b9acb9eadfcfd4e7936ca0b5707b
vibecoder11200 <vibecoder11200@gmail.com>
2026-04-04 22:41:28 +0000

    Fix CI: add missing TeamFactory and PushSubscriptionFactory

---
+++ database/factories/PushSubscriptionFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\PushSubscription;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class PushSubscriptionFactory extends Factory
+{
+    protected $model = PushSubscription::class;
+
+    public function definition(): array
+    {
+        return [
+            'user_id' => User::factory(),
+            'endpoint' => 'https://fcm.googleapis.com/fcm/send/' . $this->faker->uuid(),
+            'public_key' => base64_encode(random_bytes(32)),
+            'auth_token' => base64_encode(random_bytes(16)),
+            'content_encoding' => 'aes128gcm',
+        ];
+    }
+}
